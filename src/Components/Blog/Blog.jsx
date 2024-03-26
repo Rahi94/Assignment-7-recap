fc13b5daa70fb6ba1c6515fcf0105fddfbd04b71
tasib94 <rahi84974@gmail.com>
2024-03-27 02:40:14 +0600

    proptypes added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,16 @@
 
-const Blog = () => {
+import PropTypes from 'prop-types';
+const Blog = ({ blog }) => {
+    console.log(blog)
     return (
         <div>
-            
+
         </div>
     );
 };
+
+Blog.propTypes ={
+    blog: PropTypes.object.isRequired
+}
 
 export default Blog;
